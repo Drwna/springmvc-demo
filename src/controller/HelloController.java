79837905853d2a197d1f32780c90f2e9974f97a0
draganet <3417237276@qq.com>
2022-03-07 18:10:32 +0800

    注解 HeloController

--- src/controller/HelloController.java
+++ src/controller/HelloController.java
@@ -3,13 +3,25 @@ package controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-public class HelloController implements Controller {
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ModelAndView mav1 = new ModelAndView("hello");
 		mav1.addObject("message1", "hello, java");
 		return mav1;
 	}
 }
+
+
+//public class HelloController implements Controller {
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		ModelAndView mav1 = new ModelAndView("hello");
+//		mav1.addObject("message1", "hello, java");
+//		return mav1;
+//	}
+//}
